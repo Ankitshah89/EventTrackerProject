4d740cf701140e883d4936005283c28d5875de7e
Ankitshah89 <shahdankit@gmail.com>
2020-04-10 16:25:50 -0600

    j-units successful

---
+++ JPACovidTracker/src/test/java/com/skilldistillery/covid/entities/cases/CasesTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.covid.entities.cases;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CasesTest {
+	
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private Cases cases;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CovidTrackerPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+		
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		cases = em.find(Cases.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		cases = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(cases);
+		assertEquals("United States", cases.getName());
+	}
+
+}
