e1963815deb0b812878edc320a785c920734a643
Ankitshah89 <shahdankit@gmail.com>
2020-04-13 08:38:51 -0600

    updates

--- CovidTracker/src/main/java/com/skilldistillery/covid/CovidTrackerApplication.java
+++ CovidTracker/src/main/java/com/skilldistillery/covid/CovidTrackerApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.covid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CovidTrackerApplication {
+public class CovidTrackerApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(CovidTrackerApplication.class);
+	  } 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidTrackerApplication.class, args);
